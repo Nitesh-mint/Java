51cf925ccf29adde79dea4a7314e184402212e98
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-12 16:50:34 +0545

    initial commit

---
+++ 02_Day/types_of_variables.java
@@ -0,0 +1,13 @@
+
+public class types_of_variables {
+    static int a = 9; // this is a static variable
+    void main(){
+       int b = 6; // this is a local variable
+    }
+
+    public static void main(String args[]){ // this is a class
+        int c = 7; // this is a instance variable
+    }
+
+    
+}
