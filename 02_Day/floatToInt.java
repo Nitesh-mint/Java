87b255e10eb55fe1199c441509854f1eeaa4120b
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-12 19:40:56 +0545

    initial commit

---
+++ 02_Day/floatToInt.java
@@ -0,0 +1,8 @@
+public class floatToInt {
+    public static void main(String args[]){
+        float f =  5.6f;
+        //int a = f compile time error
+        int i = (int)f;
+        System.out.println(i);
+    }
+}
---
+++ 02_Day/intToFloat.java
@@ -0,0 +1,9 @@
+class intToFloat {
+    public static void main(String args[]){
+        int a = 5; // local integer varaible
+        float b = a;
+        System.out.println(a);//prints int
+        System.out.println(b);//prints floats
+
+    }
+}
