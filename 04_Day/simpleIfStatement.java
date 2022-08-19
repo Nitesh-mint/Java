dc4c7857b4e57af0e8d3edefd077c9b07fb3210a
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-19 18:16:12 +0545

    initial commit

---
+++ 04_Day/simpleIfStatement.java
@@ -0,0 +1,12 @@
+public class simpleIfStatement {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 5;
+        if (a > b){
+            System.out.println("error");
+        }
+        else{
+            System.out.println("They are equal.");
+        }
+    }
+}
---
+++ 04_Day/switchStatement.java
@@ -0,0 +1,19 @@
+public class switchStatement implements Cloneable{
+    public static void main(String args[]){
+        int num = 2;
+        switch (num){
+            case 0:
+            System.out.println("The value is 0");
+            break;
+
+            case 1:
+            System.out.println("The value is 1");
+            break;
+
+            case 2:
+            System.out.println("The value is 2.");
+            break;
+        }
+    }
+    
+}
