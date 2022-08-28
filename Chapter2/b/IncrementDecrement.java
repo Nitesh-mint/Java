023abb3a4741339481ce6e786e4ac0047840ddb0
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-28 20:43:23 +0545

    initial commit

---
+++ Chapter2/b/IncrementDecrement.java
@@ -0,0 +1,11 @@
+public class IncrementDecrement {
+    public static void main(String args[]){
+        int a=1;
+        System.out.println("a is:" +a); //defatult 1.
+        System.out.println("++a is:"+ ++a); //pre-increment
+        System.out.println("a++ is:"+ a++); //post-increment
+        System.out.println("a is:" +a);
+        System.out.println("--a is:"+ --a); //pre-decrement
+        System.out.println("a-- is:"+ a--);    //post-decrement
+    }
+}
