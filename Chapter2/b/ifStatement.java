bdecf35d6db68cfbf927e95f93cd758691df944a
Nitesh-mint <nitesh.raya058@gmail.com>
2022-08-29 06:51:20 +0545

    initial commit

---
+++ Chapter2/b/ifStatement.java
@@ -0,0 +1,11 @@
+public class ifStatement {
+    public static void main(String args[]){
+        String Lover1 = "Ram";
+        String Lover2 = "Sita";
+
+        if(Lover1=="Ram" && Lover2 =="Sita"){
+           System.out.println("Both of them "+Lover1 + " and " + Lover2+ " love each other so The Bihe is succesfull.");
+        }
+    }
+}
+ 
