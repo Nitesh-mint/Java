36d5968894388f9a7d539522200579a64e2fe4df
Nitesh-mint <nitesh.raya058@gmail.com>
2022-09-09 14:12:29 +0545

    initial commit

---
+++ Class/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Demo{
+	public static void main(String args[]){
+		int i,j;
+		Scanner in =  new Scanner(System.in);
+		System.out.println("Enter the size of the array:");
+		int n =  in.nextInt();
+		int [] array = new int[n];
+		
+
+		
+		System.out.println("Enter the elements of the array:");
+		for(i=0;i<n;i++){
+			array[i] = in.nextInt();
+		}
+		
+		System.out.println("Array are: ");
+		for(i=0; i<n;i++)
+		System.out.println(array[i]);
+	}
+}
+	
---
+++ Class/PrimeNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PrimeNum{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int i=1,n;
+		System.out.println("Enter the Number of prime:");
+		n = sc.nextInt();
+		while(i<=n){
+			for(i<=
+			if(i%2==0){
+				System.out.println("prime.");
+				}
+			else{
+				System.out.println("Not prime");
+				}
+			}
+}
