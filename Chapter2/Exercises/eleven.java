e918683b95c325e63243353ef061dad353984ae0
Nitesh-mint <nitesh.raya058@gmail.com>
2022-09-17 07:15:46 +0545

    initial commit

--- Chapter2/Exercises/eleven.java
+++ Chapter2/Exercises/eleven.java
@@ -6,26 +6,17 @@ public class eleven {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a character between a-z or A-Z");
         input = sc.next().charAt(0); // to read the first letter.
-        char array[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         char vowel[] = {'a','e' ,'i', 'o', 'u'};
-
-        
-        for (char x : array){
-            if(input != x){
-                System.out.println(input+ " is not valid.");
-                break;
-            }
-            else{
-                for(char y: vowel){
-                    if(input == y){
-                        System.out.println("The letter is valid.");
-                    }
-                    else{
-                        System.out.println("The letter is consonant.");
-                    }
+        if((input >='a' && input<='z') || (input>='A' && input<='Z')){
+            System.out.println(input+" is alphabet.");
+            for(char x: vowel){
+                if(input == x){
+                    System.out.println(input+ " is vowel.");
                 }
             }
-
-            }
+        }
+        else{
+            System.out.println("Nothig other than character is valid.");
+        }
         }
 }
