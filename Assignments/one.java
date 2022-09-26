1d6a6118ecb2985e63d7d4689cb8a6ab8c7d651d
Nitesh-mint <nitesh.raya058@gmail.com>
2022-09-26 19:50:57 +0545

    inital commit

---
+++ Assignments/one.java
@@ -0,0 +1,11 @@
+//program to print even number form 1 to 100
+public class one{
+    public static void main(String args[]){
+        int i;
+        for(i=1;i<=100;++i){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
