23bef6caa2eac34fca022741d0b373730607f0c2
Nitesh-mint <nitesh.raya058@gmail.com>
2022-12-27 07:33:53 +0545

    initial commit

---
+++ Problems/factorial.java
@@ -0,0 +1,9 @@
+public class factorial {
+    public static void main(String[] args) {
+        int a =5,result=1;
+        for(int i=a;i>0;i--){
+            result = result * i;
+        }
+        System.out.println(result);
+    }
+}
---
+++ lab_report/addDates.java
@@ -0,0 +1,7 @@
+import java.sql.Date;
+
+public class addDates{
+    public static void main(String[] args) {
+        Date d1 = new Date();
+    }
+}
