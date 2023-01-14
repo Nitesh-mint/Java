0dfdf8169e9d16b00e7ac65be13423b843c5069b
Nitesh-mint <nitesh.raya058@gmail.com>
2023-01-14 09:08:11 +0545

    initial commit

--- Collection Framework/Iteratorp.java
+++ Collection Framework/Iteratorp.java
@@ -3,13 +3,18 @@ import java.util.Iterator;
 public class Iteratorp {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<Integer>();
+        int ar[] = {5,6,7,8,9};
         arr.add(5);   
         arr.add(6);   
         arr.add(4);   
-        Iterator<Integer> i = arr.iterator();
-        while(i.hasNext()){
-            System.out.println(i.next());
+        Iterator<Integer> j = arr.iterator();
+        // while(i.hasNext()){
+        //     System.out.println(i.next());
+        // }
+        // System.out.println();
+        Iterator<Integer> k = arr.iterator();
+        while(k.hasNext()){
+            System.out.println(k.next());
         }
-        System.out.println();
     }
 }
